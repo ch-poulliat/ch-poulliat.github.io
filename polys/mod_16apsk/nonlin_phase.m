2d90a665b92a417c3d229d408947288bd7b2d254
Charly POULLIAT <charly.poulliat@enseeiht.fr>
2022-11-28 11:55:27 +0100

    update

--- polys/mod_16apsk/nonlin_phase.m
+++
@@ -1,5 +0,0 @@
-function y = nonlin_phase(IBO_dB, alpha_phi,beta_phi) 
-% Provides the phase value at a certain input back-off based on the Am-PM
-% parameters 
-ibo = 10^(-IBO_dB/20); 
-y = (alpha_phi.*(ibo.^2))./(1 + (beta_phi.*ibo.^2)); 
